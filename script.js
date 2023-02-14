776f01fc935a75a294ff76b8788ab7b7daea812e
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-14 13:59:48 +0300

    fix:add map method

--- script.js
+++ script.js
@@ -164,6 +164,58 @@ let result = nums.map(multiplyByTwo);
 console.log(result);
  */
 
+//!=============== своя реализация метода map========
+
+//Map
+//
+// let newArray = arr.map(callback(currentValue[, index[, array]]) {
+    // return element for newArray, after executing something
+// }[, thisArg]);
+
+/*
+let array2 = [4, 6, 8, 9, 12, 53, -17, 2, 5, 7, 31, 97, -1, 17];
+
+Array.prototype.map2 = function (callback, thisArg) {
+	if (this == null) {
+		throw new Error("Cant iterate over undefined or null");
+	}
+	let context = this;
+
+	let O = Object(this);
+
+	if (arguments.length > 1) {
+		context = thisArg;
+	}
+
+	if (typeof callback !== "function") {
+		throw new Error("Callback is not a function");
+	}
+
+	let len = O.length;
+
+	let newArray = [];
+
+	let i = 0;
+
+	while (i < len) {
+		if (i in O) {
+			newArray[i] = callback.call(context, this[i], i, O);
+		}
+
+		i++;
+	}
+
+	return newArray;
+};
+
+let increase1 = (num) => num + 1;
+let mul2 = (num) => num * 2;
+
+console.log(array2.filter2(isPrime));
+console.log(array2.filter2(isPrime).map2(mul2).map2(increase1));
+*/
+
+
 //=================================================
 
 //! метод reduce
@@ -196,3 +248,4 @@ let  fruits = array.reduce((acc, el) => {
 
 console.log(fruits);
 */
+
