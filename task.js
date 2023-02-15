6adafbdfb6728450884c84eff93d6bb3b799978c
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-15 20:37:17 +0300

    fix:add task Sum without highest and lowest number

--- task.js
+++ task.js
@@ -83,6 +83,39 @@ function smash (words) {
 };
  */
 
+//!- Sum without highest and lowest number
+
+/*
+Task
+Sum all the numbers of a given array ( cq. list ),
+except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge,
+even if there are more than one with the same value.
+
+Mind the input validation.
+ */
+
+//! Solution
+
+/*
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a,b) {return a - b;});
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
+
+ */
+
 
 
 
