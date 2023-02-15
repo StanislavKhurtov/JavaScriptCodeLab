4efdbb4bddc665b49c124b860dfa44d663083c61
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-15 20:17:32 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -34,6 +34,22 @@ function bmi(weight, height) {
 }
 */
 
+//!-Convert boolean values to strings 'Yes' or 'No'.
+
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ */
+
+/*
+function boolToWord( bool ){
+  if (bool) {
+    return 'Yes';
+  } else {
+    return 'No';
+  }
+}
+*/
+
 
 
 
