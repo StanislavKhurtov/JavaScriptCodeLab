c82aa418b1c32e17b8fcc09dc5f946adb144e362
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-16 14:53:26 +0300

    fix: add task You only need one - Beginner

--- task.js
+++ task.js
@@ -132,6 +132,20 @@ const min = (list) => Math.min(...list);
 const max = (list) => Math.max(...list);
  */
 
+//!- You only need one - Beginner
+
+/*
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+ */
+
+/*
+let check = (a, x) => {return a.includes(x)};
+ */
+
 
 
 
