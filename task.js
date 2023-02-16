d693d5ce018a108774fef86a504b38b30f3c821d
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-16 14:49:04 +0300

    add task Find Maximum and Minimum Values of a List

--- task.js
+++ task.js
@@ -99,6 +99,9 @@ Mind the input validation.
 //! Solution
 
 /*
+
+
+
 function sumArray(array) {
   if (array == null) {
     return 0;
@@ -113,7 +116,20 @@ function sumArray(array) {
     return total;
   }
 }
+ */
 
+
+//!-Find Maximum and Minimum Values of a List
+
+/*
+Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language )
+that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+ */
+
+
+/*
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
  */
 
 
