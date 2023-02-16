42c6f1eb22d13407b807df4c53f30fa9a09326ac
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-16 16:53:37 +0300

    fix: add task Invert values

--- task.js
+++ task.js
@@ -162,5 +162,22 @@ function findShort(s){
  */
 
 
+//!- Invert values
+
+/*
+Given a set of numbers, return the additive inverse of each.
+Each positive becomes negatives, and the negatives become positives.
+ */
+
+/*
+const invert = array => array.map(num => -num);
+ */
+
+//!- Isograms
+
+/*
+
+ */
+
 
 
