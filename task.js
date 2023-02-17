8c32369580cbc5bc7b15b7e6c4202065bac6d82a
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-17 20:21:38 +0300

    fix: add task n-th-fibonacci

--- task.js
+++ task.js
@@ -272,5 +272,48 @@ function duplicateEncode(word){
  */
 
 
+//! - N-th Fibonacci
+
+/*
+I love Fibonacci numbers in general, but I must admit I love some more than others.
+
+I would like for you to write me a function that, when given a number n (n >= 1 ),
+returns the nth number in the Fibonacci Sequence.
+
+For example:
+
+   nthFibo(4) == 2
+Because 2 is the 4th number in the Fibonacci Sequence.
+
+For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of
+the previous two.
+ */
+
+/*
+function nthFibo(n) {
+
+  let arr = [0, 1];
+  let result;
+
+  for (let i = 0; i < n; i++) {
+
+    if (i > 1) {
+
+      let num1 = arr[i - 2];
+      let num2 = arr[i - 1];
+      arr.push(num1 + num2);
+    }
+
+    if (i + 1 === n) {
+      result = arr[i];
+    };
+  };
+
+  return result;
+};
+ */
+
+
+
 
 
