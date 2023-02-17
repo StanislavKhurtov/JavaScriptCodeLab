ac157359ff552eb2ced8ffdfd5d223b787fc8681
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-17 20:17:38 +0300

    fix: add file Duplicate Encoder

--- task.js
+++ task.js
@@ -237,3 +237,40 @@ function getParticipants(handshakes){
  */
 
 
+//!- Duplicate Encoder
+
+/*
+The goal of this exercise is to convert a string to a new string where each character in the new string
+is "(" if that character appears only once in the original string, or ")" if that character appears more than once in
+the original string. Ignore capitalization when determining if a character is a duplicate.
+
+ */
+
+/*
+function duplicateEncode(word){
+
+  let charCount = {};
+
+  let encodedStr = "";
+
+  for (let i = 0; i < word.length; i++) {
+
+    let char = word[i].toLowerCase();
+
+    charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
+  }
+
+  for (let i = 0; i < word.length; i++) {
+
+    let char = word[i].toLowerCase();
+
+    encodedStr += charCount[char] > 1 ? ")" : "(";
+  }
+
+  return encodedStr;
+}
+ */
+
+
+
+
