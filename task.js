664a1062dd2e8b176b511de50d4007908a494dfe
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-18 19:52:59 +0300

    fix: add task Count by X

--- task.js
+++ task.js
@@ -345,6 +345,45 @@ return x.reduce((acc,num) => {
 }
  */
 
+//! - Count by X
+
+/*
+Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+ */
+
+/*
+function countBy(x, n) {
+  let z = [];
+
+   for (let i = 1; i <= n; i++) {
+
+    z.push(i * x);
+  }
+
+
+  return z;
+}
+
+ */
+
+//!- If you can't sleep, just count sheep!!
+
+/*
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...".
+Input will always be valid, i.e. no negative integers.
+ */
+
+/*
+
+ */
+
 
 
 
