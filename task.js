4b11c46e4a8b521ce94f460123deaf3f3af12458
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-18 19:38:01 +0300

    fix: add task Sum Mixed Array

--- task.js
+++ task.js
@@ -314,6 +314,21 @@ function nthFibo(n) {
  */
 
 
+//! - Sum Mixed Array
+
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+ */
+
+/*
+function sumMix(x){
+  return x.map(element => Number(element)).reduce((sum, num) => sum += num);
+}
+ */
+
+
 
 
 
