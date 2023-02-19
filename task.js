a4a2eded71db79f03f032c3e7e59f51450b43b02
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-19 11:23:19 +0300

    fix: add task Length of missing array (6)

--- task.js
+++ task.js
@@ -440,6 +440,35 @@ function deepCount(a){
  */
 
 
+//!- Length of missing array
+/*
+You get an array of arrays.
+If you sort the arrays by their length, you will see, that their length-values are consecutive.
+But one array is missing!
+
+
+You have to write a method, that return the length of the missing array.
+ */
+
+/*
+function getLengthOfMissingArray(arrayOfArrays) {
+  const lengths = (arrayOfArrays || [])
+    .map(a => a ? a.length : 0)
+    .sort((a, b) => a - b)
+
+  if (lengths.includes(0)) {
+    return 0
+  }
+
+  for (let i = 0; i < lengths.length - 1; i++) {
+    if (lengths[i] + 1 !== lengths[i + 1]) {
+      return lengths[i] + 1
+    }
+  }
+
+  return 0
+}
+ */
 
 
 
