f5d598678f596cc7fb23a80f8258f4e99a60ca94
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-23 15:47:38 +0300

    fix: add task Sum The Strings

--- task.js
+++ task.js
@@ -886,3 +886,29 @@ If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 /*
 const hoopCount = n => n > 9 ? "Great, now move on to tricks" : "Keep at it until you get it";
  */
+
+//! Sum The Strings
+
+/*
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+Notes:
+
+If either input is an empty string, consider it as zero.
+
+Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+ */
+
+/*
+function sumStr(a,b) {
+  return String(Number(a)+Number(b));
+}
+ */
+
