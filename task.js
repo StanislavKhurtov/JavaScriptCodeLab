1cf9780c8856c6c1ace82a870e5b406a1469cd9e
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-23 15:43:36 +0300

    fix:add task Are You Playing Banjo?

--- task.js
+++ task.js
@@ -852,3 +852,20 @@ function points(games) {
 }
  */
 
+//! Are You Playing Banjo?
+
+/*
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo"
+name + " does not play banjo"
+Names given are always valid strings.
+ */
+
+
+/*
+const areYouPlayingBanjo = (n) => n[0].toLowerCase() === "r" ? n + " plays banjo" : n + " does not play banjo";
+ */
