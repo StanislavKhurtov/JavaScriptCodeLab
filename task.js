c7283efc96c0caa6b3693c88adf3b2310467eb36
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-24 10:59:41 +0300

    fix: add task Sort and Star

--- task.js
+++ task.js
@@ -978,3 +978,19 @@ Note! The input is a Real number (actual type is language dependent) and is >= 0
 /*
 const cockroachSpeed = s => Math.floor(s / 0.036);
  */
+
+//! Sort and Star
+
+/*
+You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of
+the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+ */
+
+/*
+const twoSort = a => a.sort()[0].split("").join("***");
+ */
+
