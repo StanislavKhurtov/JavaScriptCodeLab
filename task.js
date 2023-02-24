6b4a6cb98bbfb2f86b38ce0d53c133dc3d53d62d
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-24 11:01:30 +0300

    fix: add task Abbreviate a Two Word Name

--- task.js
+++ task.js
@@ -994,3 +994,26 @@ You should not remove or add elements from/to the array.
 const twoSort = a => a.sort()[0].split("").join("***");
  */
 
+//! Abbreviate a Two Word Name
+
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+ */
+
+/*
+function abbrevName(name){
+
+    return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+
+}
+ */
+
+//!
