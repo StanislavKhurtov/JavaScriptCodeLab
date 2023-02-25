7401f9eb07e7c834f9ad2814c2de8e28ab2d8163
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-25 16:11:23 +0300

    fix: add task Convert number to reversed array

--- task.js
+++ task.js
@@ -1150,3 +1150,30 @@ function solution(str){
 }
  */
 
+
+//! Convert number to reversed array of digits
+
+/*
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example(Input => Output):
+35231 => [1,3,2,5,3]
+0 => [0]
+ */
+
+/*
+function digitize(n) {
+
+  let arr = String(n).split("").reverse();
+
+  arr.forEach(function(numStr, idx) {
+    arr[idx] = Number(numStr);
+  });
+
+  return arr;
+}
+ */
+
+//!
+
