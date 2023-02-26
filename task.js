eeacbab3150eae9a9874a167d1ffe4e660ed7853
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-26 10:27:34 +0300

    fix: add task Is the string uppercase?

--- task.js
+++ task.js
@@ -1346,5 +1346,27 @@ function betterThanAverage(classPoints, yourPoints) {
  */
 
 
-//!
+//!Is the string uppercase?
+
+/*
+Is the string uppercase?
+Task
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+ */
+
+/*
+String.prototype.isUpperCase = function() {
+  let str = String(this);
+
+  return str === str.toUpperCase();
+}
+ */
 
