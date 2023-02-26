291ff607aa8e05fa83e2cc84c4ce7bfd74d7f185
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-26 10:16:48 +0300

    fix: add task  Keep Hydrated!

--- task.js
+++ task.js
@@ -1285,4 +1285,29 @@ function basicOp(operation, value1, value2) {
 
  */
 
-//!
+//! Keep Hydrated!
+
+/*
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the
+smallest value.
+
+For example:
+
+time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres =
+ */
+
+/*
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+ */
+
+//! 
