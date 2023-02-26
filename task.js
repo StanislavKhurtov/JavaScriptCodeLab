48f1312282170f39b74f678bb7f94a36ceebaf96
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-26 10:20:47 +0300

    fix: add task  Convert a Boolean to a String

--- task.js
+++ task.js
@@ -1310,4 +1310,18 @@ function litres(time) {
 }
  */
 
-//! 
+//! Convert a Boolean to a String
+
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+ */
+
+/*
+function booleanToString(b){
+  return b.toString();
+}
+ */
+
+
