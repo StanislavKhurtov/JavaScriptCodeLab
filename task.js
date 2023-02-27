1ec3c2368404462879c61717b5ee084f38923a9b
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-27 13:23:44 +0300

    fix: add task Parse nice int from char problem

--- task.js
+++ task.js
@@ -1492,4 +1492,21 @@ Examples
 const getRealFloor = n => n < 1 ? n : n < 13 ? n - 1 : n - 2;
  */
 
+//!Parse nice int from char problem
+
+/*
+You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old".
+ The first character in the string is always a number.
+ */
+
+/*
+function getAge(inputString){
+  return parseInt(inputString);
+}
+ */
+
 //!
