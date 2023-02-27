c1ec080c204deef0b7dd89f9b87e372cc5f8e2a3
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-27 13:10:35 +0300

    fix:add task Sum of positive

--- task.js
+++ task.js
@@ -1346,7 +1346,7 @@ function betterThanAverage(classPoints, yourPoints) {
  */
 
 
-//!Is the string uppercase?
+//! Is the string uppercase?
 
 /*
 Is the string uppercase?
@@ -1370,3 +1370,19 @@ String.prototype.isUpperCase = function() {
 }
  */
 
+
+//! Sum of positive
+
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+ */
+
+/*
+function positiveSum(arr) {
+  return arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
+}
+ */
