de3374fc44ccf5d20efc531c93e3c39a193b2a9e
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-28 15:19:53 +0300

    fix: add task Beginner - Lost Without a Map

--- task.js
+++ task.js
@@ -1659,4 +1659,18 @@ function removeExclamationMarks(s) {
 }
  */
 
-//! 
+//! Beginner - Lost Without a Map
+
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+ */
+
+/*
+function maps(x){
+  return x.map(n => n * 2);
+}
+ */
