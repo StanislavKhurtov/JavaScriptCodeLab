df517e4beac79ba4a99d95024ed1c9744a240bc8
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-28 15:12:17 +0300

    fix: add task Find the smallest integer in the arr

--- task.js
+++ task.js
@@ -1570,4 +1570,38 @@ function isPalindrome(x) {
 }
  */
 
-//! 
+//! Find the smallest integer in the array
+
+/*
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+ */
+
+/*
+class SmallestIntegerFinder {
+
+    findSmallestInt(args) {
+
+      let smallestInt = args[0];
+
+      for (let i = 0; i < args.length; i++) {
+
+        let currNum = args[i];
+
+        if (currNum < smallestInt) {
+
+          smallestInt = currNum;
+        }
+      }
+
+    return smallestInt;
+  }
+}
+ */
+
+//!
