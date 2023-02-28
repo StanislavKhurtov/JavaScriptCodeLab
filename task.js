72c610be9b9e87f4561ec2675b3c242959cd1e64
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-02-28 15:18:00 +0300

    fix:add task Remove exclamation marks

--- task.js
+++ task.js
@@ -1647,4 +1647,16 @@ function sayHello(name) {
 }
  */
 
-//!
+//!Remove exclamation marks
+
+/*
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+ */
+
+/*
+function removeExclamationMarks(s) {
+  return s.replace(/!/gi, '');
+}
+ */
+
+//! 
