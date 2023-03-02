28a5f9c043cc8a11c2c16bda2cf106847ff60f4a
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-02 14:25:29 +0300

    fix:add task A wolf in sheep's clothing

--- task.js
+++ task.js
@@ -1924,4 +1924,30 @@ function reverseList(list) {
 }
  */
 
+//! A wolf in sheep's clothing
+
+
+/*
+Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now
+plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+Warn the sheep in front of the wolf that it is about to be eaten. Remember that you
+are standing at the front of the queue which is at the end of the array:
+
+[sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+   7      6      5      4      3
+ */
+
+/*
+function warnTheSheep(queue) {
+
+  let wolfIdx = queue.reverse().indexOf("wolf");
+
+  return wolfIdx === 0 ?
+    "Pls go away and stop eating my sheep" :
+    "Oi! Sheep number " + wolfIdx +"! You are about to be eaten by a wolf!";
+}
+ */
+
+
 //! 
