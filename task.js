28a6cf6b3135755c4a3001a667a9686e11668ae0
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-02 14:03:47 +0300

    fix:add task What is between?

--- task.js
+++ task.js
@@ -1815,3 +1815,32 @@ function getGrade (s1, s2, s3) {
   return "A";
 }
  */
+
+//!What is between?
+
+/*
+Complete the function that takes two integers (a, b, where a < b)
+and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+ */
+
+/*
+function between(a, b) {
+  let arr = [];
+
+  for (let i = a; i <= b; i++) {
+
+    arr.push(i);
+  }
+
+  return arr;
+}
+ */
+
+
+// !
