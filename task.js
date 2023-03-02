62b4ad0fa5bddf1d4141d81e32b0e18e24e7f900
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-02 14:01:27 +0300

    fix: add task Grasshopper - Grade book

--- task.js
+++ task.js
@@ -1089,7 +1089,8 @@ If you can, try writing it in one line of code.
 /*
 
 function find_difference(a, b) {
-return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
+return Math.abs(a.reduce((previous, current) => previous * current) - b
+.reduce((previous, current) => previous * current));
 }
  */
 
@@ -1782,4 +1783,35 @@ function powersOfTwo(n){
 }
  */
 
-//!
+//!Grasshopper - Grade book
+
+/*
+Complete the function so that it finds the average of the three scores passed to
+it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative
+values or values greater than 100.
+ */
+
+/*
+function getGrade (s1, s2, s3) {
+
+  let mean = (s1 + s2 + s3) / 3;
+  let grades = [60, 70, 80, 90, 100];
+
+  for (let i = 0; i < grades.length; i++) {
+
+    if (mean < grades[i]) {
+      return "FDCBA"[i];
+    }
+  }
+
+  return "A";
+}
+ */
