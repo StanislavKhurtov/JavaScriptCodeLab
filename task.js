529443aad504daa0f64d41fc135fbc1a97a422da
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-03 10:40:59 +0300

    fix: add task Drink about

--- task.js
+++ task.js
@@ -2026,4 +2026,45 @@ function lovefunc(flower1, flower2){
 }
  */
 
-//! 
+//! Drink about
+
+/*
+Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+Examples: (Input --> Output)
+
+13 --> "drink toddy"
+17 --> "drink coke"
+18 --> "drink beer"
+20 --> "drink beer"
+30 --> "drink whisky"
+ */
+
+/*
+function peopleWithAgeDrink(old) {
+
+  let drinks = {
+    14: "toddy",
+    18: "coke",
+    21: "beer",
+  };
+
+  let allAges = Object.keys(drinks).map(str => Number(str));
+
+  let age = allAges.filter(n => old < n)[0];
+
+  return "drink " + (age ? drinks[age] : "whisky");
+};
+ */
+
+//!
