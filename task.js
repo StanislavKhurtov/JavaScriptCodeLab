82006edb93e1e5f19ed204feba0e6e09ca9c32fe
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-04 12:26:46 +0300

    fix: add task Grasshopper - Messi Goals

--- task.js
+++ task.js
@@ -2093,4 +2093,27 @@ For example, a multiplication table (string) for number == 5 looks like below:
 const multiTable = num => Array(10).fill(num).map((n, i) => `${i+1} * ${n} = ${(i+1) * n}`).join("\n");
  */
 
-//! 
+//! Grasshopper - Messi Goals
+
+/*
+Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+ */
+
+/*
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+ */
+
+//!
