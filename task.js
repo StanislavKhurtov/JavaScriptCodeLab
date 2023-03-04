03cb603f1b1d5aec2b33393ad36d14f3601a9d69
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-04 12:34:17 +0300

    fix: add task Training JS #2: Basic data types

--- task.js
+++ task.js
@@ -2171,4 +2171,43 @@ function take(arr, n) {
  */
 
 
-//! 
+//! Training JS #2: Basic data types--Number
+
+/*
+In javascript, Number is one of basic data types. It can be positive: 1,2,3,
+negative:-1,-100 , integer:123,456, decimal:3.1415926,-8.88 etc..
+
+Numbers can use operators such as + - * / %
+ */
+
+/*
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+  var a=v1
+  var b=v1
+  return a+b;
+}
+//Please refer to the example above to complete the following functions
+function equal2(){
+  var a= v3  //set number value to a
+  var b=  v1 //set number value to b
+  return a-b;
+}
+function equal3(){
+  var a= v1  //set number value to a
+  var b= v5 //set number value to b
+  return a*b;
+}
+function equal4(){
+  var a=  v4 //set number value to a
+  var b=  v5 //set number value to b
+  return a/b;
+}
+function equal5(){
+  var a=  v2 //set number value to a
+  var b=  v3 //set number value to b
+  return a%b;
+}
+ */
+
+//!
