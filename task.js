97f25b2bf13324cabdfccebb3699bb094a3ef35b
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-04 12:32:18 +0300

    fix: add task Enumerable Magic #25 - Take the Fir

--- task.js
+++ task.js
@@ -2155,4 +2155,20 @@ function plural(n) {
 }
  */
 
-//!
+//!Enumerable Magic #25 - Take the First N Element
+
+/*
+Create a function that accepts a list/array and a number n,
+and returns a list/array of the first n elements from the list/array.
+
+If you need help, here's a reference:
+ */
+
+/*
+function take(arr, n) {
+  return arr.slice(0, n);
+}
+ */
+
+
+//! 
