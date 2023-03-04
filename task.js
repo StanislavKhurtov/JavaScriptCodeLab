977f3db66c07831e476aa0fa903ff6673969d7df
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-04 12:36:09 +0300

    fix: add task Exclamation marks series #11

--- task.js
+++ task.js
@@ -2210,4 +2210,24 @@ function equal5(){
 }
  */
 
-//!
+//!Exclamation marks series #11
+
+/*
+DESCRIPTION:
+Description:
+Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+Examples
+replace("Hi!") === "H!!"
+replace("!Hi! Hi!") === "!H!! H!!"
+replace("aeiou") === "!!!!!"
+replace("ABCDE") === "!BCD!"
+ */
+
+/*
+function replace(s){
+  return s.replace(/[aeoiu]/ig, '!')
+}
+ */
+
+//! 
