48c69c854c1a7924f0fdc4b0442a660c5ca71a5c
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-05 11:35:21 +0300

    fix: add task  Hex to Decimal

--- task.js
+++ task.js
@@ -2533,3 +2533,18 @@ if( a > b && b === 0) {
 
 }
  */
+
+
+//! Hex to Decimal
+/*
+Complete the function which converts hex number (given as a string) to a decimal number.
+ */
+
+/*
+
+function hexToDec(hexString){
+ return  parseInt(hexString, 16);
+}
+ */
+
+//!
