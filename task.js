0a0f8607bc44afbcfbcffe16c200d9b54d5deb2b
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-05 11:32:51 +0300

    fix: add task Find the Remainder

--- task.js
+++ task.js
@@ -2513,4 +2513,23 @@ function rainAmount(mm){
  */
 
 
-//! 
+//! Find the Remainder
+
+/*
+Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+Division by zero should return NaN.
+ */
+
+/*
+function remainder(a, b){
+  // Divide the larger argument by the smaller argument and return the remainder
+if( a > b && b === 0) {
+  return NaN;
+} else if ( b > a && a === 0) {
+ return NaN;
+}
+  return (a > b )? a % b : b % a;
+
+}
+ */
