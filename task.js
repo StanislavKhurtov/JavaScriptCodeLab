19a1219d864452effa4beb4983e8797f97f682e9
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-06 15:07:35 +0300

    fix: add task Small enough? - Beginner

--- task.js
+++ task.js
@@ -2547,4 +2547,17 @@ function hexToDec(hexString){
 }
  */
 
-//!
+//Small enough? - Beginner
+
+/*
+You will be given an array and a limit value. You must check that all values in
+the array are below or equal to the limit value. If they are, return true. Else,
+return false.
+
+You can assume all values in the array are numbers.
+ */
+/*
+// !const smallEnough = (a, limit) => a.filter(n => n > limit).length === 0;
+*/
+
+//! 
