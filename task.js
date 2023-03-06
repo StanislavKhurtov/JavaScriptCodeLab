b8be398eb62d4392b8f797dbc65c5c7c29f98f98
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-06 15:22:37 +0300

    fix: add task Find the position!

--- task.js
+++ task.js
@@ -2642,4 +2642,19 @@ function distinct(a) {
 }
  */
 
-//!
+//!Find the position!
+/*
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+ */
+
+/*
+function position(letter){
+  return `Position of alphabet: ${letter.charCodeAt()-96}`
+}
+ */
+
+//! 
