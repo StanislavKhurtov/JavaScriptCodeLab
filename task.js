d8fbe1e84e9a8c050a79294823c294d43f4524a1
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-07 08:43:41 +0300

    fix: add task 5 without numbers !!

--- task.js
+++ task.js
@@ -2708,4 +2708,18 @@ function animal(obj){
 }
  */
 
-//!
+//!5 without numbers !!
+
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+ */
+
+/*
+function unusualFive() {
+  return ['banan', 'orange', 'apple', 'kiwi', 'lemon'].length;
+}
+ */
