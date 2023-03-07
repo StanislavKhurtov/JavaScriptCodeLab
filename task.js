defb9ce00b30236957634a690e16ca45ca1a5f7c
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-07 08:45:32 +0300

    fix:add task No Loops 2 - You only need one

--- task.js
+++ task.js
@@ -2723,3 +2723,23 @@ function unusualFive() {
   return ['banan', 'orange', 'apple', 'kiwi', 'lemon'].length;
 }
  */
+
+//! No Loops 2 - You only need one
+
+/*
+*** No Loops Allowed ***
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value,*
+*  without using a loop.
+
+Array can contain numbers or strings. x can be either. Return true if the array contains*  the value, false if not.
+* With strings you will need to account for case.
+ */
+
+/*
+function check(a, x) {
+  return a.includes(x);
+}
+ */
+
+//! 
