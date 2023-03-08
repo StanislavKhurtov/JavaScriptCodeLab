ddb567f14a8ddaa28a551dbec726331fcd913191
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-08 10:17:39 +0300

    fisadd task Do you speak "English"?

--- task.js
+++ task.js
@@ -2942,4 +2942,32 @@ const calculator = (a,b,sign) => {
 }
  */
 
-//! 
+//! Do you speak "English"?
+
+/*
+Given a string of arbitrary length with any ascii characters. Write a function to determine
+whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef"
+is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+ */
+
+/*
+function spEng(sentence){
+
+  for (let i = 0; i < sentence.length - 6; i++) {
+
+    if (sentence.slice(i, i + 7).toLowerCase() === "english") {
+      return true;
+    }
+  }
+
+  return false;
+}
+ */
+
+//!
