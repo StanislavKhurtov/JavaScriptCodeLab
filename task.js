6826fb46ef731ff6444f064650b41c2bb1487097
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-08 10:28:40 +0300

    fix :add task Area of a Square

--- task.js
+++ task.js
@@ -3040,4 +3040,40 @@ return result
 }
  */
 
+//!How many stairs will Suzuki climb in 20 years?
+
+/*
+Suzuki is a monk who climbs a large staircase to the monastery as part of a ritual.
+Some days he climbs more stairs than others depending on the number of students he must train in the
+morning. He is curious how many stairs might be climbed over the next 20 years and has spent a year
+marking down his daily progress.
+
+The sum of all the stairs logged in a year will be used for estimating the number he might climb in 20.
+
+20_year_estimate = one_year_total * 20
+ */
+
+/*
+function stairsIn20(s){
+  return   s
+    .reduce((arr, day) => arr.concat(...day), [])
+    .reduce((total, stairs) => total + stairs, 0) * 20
+}
+ */
+
+
+//!Area of a Square
+
+/*
+Complete the function that calculates the area of the red square, when the length of the circular arc A is given
+as the input. Return the result rounded to two decimals.
+ */
+
+/*
+function squareArea(A){
+  return Number(Math.pow(2 * A / 3.1416, 2).toFixed(2))
+
+}
+ */
+
 //!
