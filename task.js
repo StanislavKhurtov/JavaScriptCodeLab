66469277f0f7c955771afe23b5aa95172b2c4b8b
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-08 10:15:47 +0300

    fix:ass task simple calculator

--- task.js
+++ task.js
@@ -2910,4 +2910,36 @@ function stringClean(s){
 }
  */
 
-//!
+//! simple calculator
+
+/*
+You are required to create a simple calculator that returns the result of addition, subtraction,
+multiplication or division of two numbers.
+
+Your function will accept three arguments:
+The first and second argument should be numbers.
+The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+if the variables are not numbers or the sign does not belong to the list above a message "unknown
+value" must be returned.
+ */
+
+/*
+const calculator = (a,b,sign) => {
+  if(sign !== '+' && sign !== '-' && sign !== '/' && sign !== '*') {
+    return 'unknown value';
+  } else if(typeof a !== 'number' || typeof b !== 'number') {
+    return 'unknown value';
+  } else if(sign === '+') {
+    return a + b;
+  } else if(sign === '-') {
+    return a - b;
+  } else if(sign === '*') {
+    return a * b;
+  } else if(sign === '/') {
+    return a / b;
+  }
+}
+ */
+
+//! 
