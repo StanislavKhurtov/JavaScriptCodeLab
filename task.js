e41ba7b111195997aa7e4166364a3fb8df6f9e7c
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-09 15:25:18 +0300

    fix:add task Training JS #8: Conditional statement

--- task.js
+++ task.js
@@ -3166,4 +3166,29 @@ function whatday(num) {
 }
  */
 
-//! 
+//! Training JS #8: Conditional statement--switch
+
+/*
+In JavaScript, switch can replace multiple if statements.
+ */
+
+/*
+function howManydays(month){
+  switch (month){
+  case 1:return 31;
+  case 2:return 28;
+  case 3:return 31;
+  case 4:return 30;
+  case 5:return 31;
+  case 6:return 30;
+  case 7:return 31;
+  case 8:return 31;
+  case 9:return 30;
+  case 10:return 31;
+  case 11:return 30;
+  case 12:return 31;
+
+  }
+  return month;
+}
+ */
