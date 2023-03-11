257087f429e392abd46dca9aeaacdba539179dde
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-11 15:07:46 +0300

    fear:add task My Language Skills

--- kyu 6.js
+++ kyu 6.js
@@ -372,3 +372,24 @@ function find(obj,path) {
 
 
 */
+
+//! My Language Skills
+
+/*
+Task
+You are given a dictionary/hash/object containing some languages and your test results in the
+given languages. Return the list of languages where your test score is at least 60, in descending
+order of the scores.
+
+Note: the scores will always be unique (so no duplicate values)
+
+Examples
+ */
+
+/*
+function myLanguages(results) {
+  return Object.keys(results)
+    .filter(v => results[v] >= 60)
+    .sort((a, b) => results[b] - results[a]);
+}
+ */
