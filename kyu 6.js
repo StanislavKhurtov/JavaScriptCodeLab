a4782eafae3d5684a4a0fc89d79cfaad502b0e01
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-12 17:40:50 +0300

    fear: add  task u JavaScript Array Filter

--- kyu 6.js
+++ kyu 6.js
@@ -393,3 +393,19 @@ function myLanguages(results) {
     .sort((a, b) => results[b] - results[a]);
 }
  */
+
+//!JavaScript Array Filter
+
+/*
+JavaScript Arrays support a filter function (starting in JavaScript 1.6).
+Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+ */
+/*
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(value=>{if(value%2==0)return value})
+}
+ */
