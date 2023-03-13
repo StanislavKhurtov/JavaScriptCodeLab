907272fe0df62480e4914b1ccfbfe32a0c4bacca
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-13 13:52:31 +0300

    feat: add task first-class function factory

--- task.js
+++ task.js
@@ -3232,3 +3232,51 @@ function array(arr){
 
 }
  */
+
+//! Jaden Casing Strings
+
+/*
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010)
+and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter.
+When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll
+ have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings
+ are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed
+ them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+ */
+
+/*
+
+ */
+
+//! 7 - First-Class Function Factory
+/*
+Write a function, factory, that takes a number as its parameter and returns another function.
+
+The returned function should take an array of numbers as its parameter, and return an array of
+those numbers multiplied by the number that was passed into the first function.
+
+In the example below, 5 is the number passed into the first function. So it returns a function
+that takes an array and multiplies all elements in it by five.
+
+Translations and comments (and upvotes) welcome!
+
+Example
+var fives = factory(5);       // returns a function - fives
+var myArray = [1, 2, 3];
+fives(myArray);               //returns [5, 10, 15];
+ */
+
+/*
+function factory(x){
+    return array => array.map( a => a*x );
+}
+ */
+
+//!
