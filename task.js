68d92febbc83b5494ae6dab7174a8b551a7d5889
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-14 14:11:50 +0300

    feat:add taskFunwithES6Classes#2-AnimalsandInherit

--- task.js
+++ task.js
@@ -3416,4 +3416,70 @@ let newFunction = function newFunc() {
 };
  */
 
-//!
+//!Fun with ES6 Classes #2 - Animals and Inheritance
+
+
+/*
+Overview
+Preloaded for you in this Kata is a class Animal:
+
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+ */
+
+/*
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.legs = 0;
+    this.status = status;
+    this.species = "shark";
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.legs = 4;
+    this.species = "cat";
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master = "") {
+    super();
+    this.master = master;
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.legs = 4;
+    this.species = "dog";
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
+ */
+
+//! 
