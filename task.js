1ccccb479eb60d08d45234ee82d87718979fca4c
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-15 15:34:26 +0300

    upload:add task Anagram Detection

--- task.js
+++ task.js
@@ -3553,4 +3553,26 @@ A string consists of lowercase latin letters, digits and symbols.
 reverseLetter=(s)=>s.replace(/[^a-z]/gi,'').split('').reverse().join('');
  */
 
-//! 
+//! Anagram Detection
+/*
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return
+false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+ */
+
+
+/*
+function isAnagram (test, original) {
+  return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
+}
+ */
+
+//!
