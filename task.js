1c8f9c4c92d704f582ea8e15a70a2feab42c8ec1
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-15 15:32:28 +0300

    upload:add task  Simple Fun #176: Reverse Letter

--- task.js
+++ task.js
@@ -3531,4 +3531,26 @@ function sumTwoSmallestNumbers(numbers) {
 };
  */
 
+//! Simple Fun #176: Reverse Letter
+
+/*
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+ */
+
+
+/*
+reverseLetter=(s)=>s.replace(/[^a-z]/gi,'').split('').reverse().join('');
+ */
+
 //! 
