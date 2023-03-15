dba5466b5669e36f7092c11c2c796847be6eef00
Stanislav <90823409+StanislavKhurtov@users.noreply.github.com>
2023-03-15 15:36:22 +0300

    uploasd:add task Categorize New Member

--- task.js
+++ task.js
@@ -3575,4 +3575,20 @@ function isAnagram (test, original) {
 }
  */
 
-//!
+//!Categorize New Member
+
+/*
+function openOrSenior(data){
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
+ */
+
+//! Categorize New Member
+
+/*
+function openOrSenior(data){
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
+ */
+
+//! 
