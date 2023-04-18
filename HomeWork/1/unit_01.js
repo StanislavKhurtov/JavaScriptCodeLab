4602e22269d3cfe0f961c76a1a70e4db9cf6496a
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-18 13:50:33 +0300

    feat: add solid task #19-20

--- HomeWork/1/unit_01.js
+++ HomeWork/1/unit_01.js
@@ -124,5 +124,13 @@
 // Task 19.
 // Создайте div c классом s3 и div с классом s4. Получите div.s3 в переменную divS3. Выведите в консоль divS3. Теперь получите в эту же переменную divS3 блок div c классом s4. Выведите переменную divS3 в консоль. Изучите что изменилось.
 
+//let divS3 = document.querySelector(".s3");
+//console.log(divS3);
+//divS3 = document.querySelector(".s4");
+//console.log(divS3);
+
+
 // Task 20
 // Получите c помощью querySelector тег body. С помощью innerHTML присвойте ему пустую строку (...innerHTML = ''). Изучите что произошло. Закомментируйте этот код.
+
+//document.querySelector('body').innerHTML = "";
