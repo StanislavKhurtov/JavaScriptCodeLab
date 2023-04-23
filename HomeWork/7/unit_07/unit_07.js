3da0c274f8e793e875d5672be4f370753d9b92f4
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-23 07:49:40 +0300

    feat: add solid file #7 task#1-2

--- HomeWork/7/unit_07/unit_07.js
+++ HomeWork/7/unit_07/unit_07.js
@@ -1,27 +1,24 @@
 // Task 1
 // Напишите функцию t1, которая при нажатии кнопки выводит в out - 1 переменную a1.
 
-let a1 = 8;
-function t1() {
-
-}
-
-document.querySelector('.b-1').onclick = t1;
+//  let a1 = 8;
+//  function t1() {
+//  	document.querySelector('.out-1').innerHTML = a1;
+//  }
+//  document.querySelector('.b-1').onclick = t1;
 
 
 // Task 2.
 
 // Изменим задачу 1. Сейчас она только выводит переменную в заранее заданный блок.Давайте сделаем так, чтобы функция, была более гибкой.Пусть теперь функция t2 возвращает переменную a2.Поскольку функция возвращает переменную, то имя функции со скобками(вызов функции) можно встраивать в выражения.Обратите внимание, как изменяется вызов функции теперь.
 
-let a2 = 8;
-function t2() {
-
-    //return
-}
-
-document.querySelector('.b-2').onclick = function () {
-    document.querySelector('.out-2').textContent = t2();
-}
+//  let a2 = 8;
+//  function t2() {
+//  	return a2;
+//  }
+//  document.querySelector('.b-2').onclick = function () {
+//  	document.querySelector('.out-2').textContent = t2();
+//  }
 
 // Task 3.
 // Наша предыдущая функция, сильно все еще зависима от внешних переменных.Давайте сделаем ее более универсальной.Пусть функция t3 принимает 2 параметра и выводит их произведение.Допишите код функции так, чтобы она возвращала произведение двух чисел, переданных ей в качестве параметра a, b.Протестируем функцию на двух примерах, с помощью кнопок b-3-1 и b-3-2.
@@ -31,21 +28,21 @@ function t3(a, b) {
 }
 
 document.querySelector('.b-3-1').onclick = function () {
-    document.querySelector('.out-3-1').textContent = t3(3, 4);
+	document.querySelector('.out-3-1').textContent = t3(3, 4);
 }
 document.querySelector('.b-3-2').onclick = function () {
-    document.querySelector('.out-3-2').textContent = t3(5, 6);
+	document.querySelector('.out-3-2').textContent = t3(5, 6);
 }
 
 // Task 4
-// Напишите функцию t4 которая принимает ваш год рождения  и вычисляет ваш возраст. 
+// Напишите функцию t4 которая принимает ваш год рождения  и вычисляет ваш возраст.
 
 function t4(year) {
 
 }
 
 document.querySelector('.b-4').onclick = function () {
-    document.querySelector('.out-4').textContent = t4(1983);
+	document.querySelector('.out-4').textContent = t4(1983);
 }
 
 
@@ -58,32 +55,32 @@ function t5(yourName) {
 }
 
 document.querySelector('.b-5').onclick = function () {
-    document.querySelector('.out-5').textContent = t5('Alex');
+	document.querySelector('.out-5').textContent = t5('Alex');
 }
 
 
 
 // Task 6
-// Напишите функцию t6, которая принимает 2 числа и возвращает случайное целое число от первого до второго принятого параметра. 
+// Напишите функцию t6, которая принимает 2 числа и возвращает случайное целое число от первого до второго принятого параметра.
 
 function t6(a, b) {
 
 }
 
 document.querySelector('.b-6').onclick = function () {
-    document.querySelector('.out-6').textContent = t6(1, 7);
+	document.querySelector('.out-6').textContent = t6(1, 7);
 }
 
 
 // Task 7
-// Напишите функцию t7, которая возвращает случайный цвет в формате rgb(x, y, z)(строка).Где x, y, z - случайные числа в диапазоне[0, 255]. 
+// Напишите функцию t7, которая возвращает случайный цвет в формате rgb(x, y, z)(строка).Где x, y, z - случайные числа в диапазоне[0, 255].
 
 function t7() {
-    //return `rgb(${t6(0,255)}....допишите сами
+	//return `rgb(${t6(0,255)}....допишите сами
 }
 
 document.querySelector('.b-7').onclick = function () {
-    document.querySelector('.out-7').style.background = t7();
+	document.querySelector('.out-7').style.background = t7();
 }
 
 
@@ -95,19 +92,19 @@ function t8(str) {
 }
 
 document.querySelector('.b-8').onclick = function () {
-    document.querySelector('.out-8').textContent = t8('  Hello  ');
+	document.querySelector('.out-8').textContent = t8('  Hello  ');
 }
 
 
 // Task 9
-// Напишите функцию t9, которая принимает число и возвращает true, если число четное, и false если не четное. 
+// Напишите функцию t9, которая принимает число и возвращает true, если число четное, и false если не четное.
 
 function t9(num) {
 
 }
 
 document.querySelector('.b-9').onclick = function () {
-    document.querySelector('.out-9').textContent = t9(15);
+	document.querySelector('.out-9').textContent = t9(15);
 }
 
 
@@ -119,5 +116,5 @@ function t10(a, b) {
 }
 
 document.querySelector('.b-10').onclick = function () {
-    document.querySelector('.out-10').textContent = t10(3, 9);
+	document.querySelector('.out-10').textContent = t10(3, 9);
 }
