56213ef7d63e77c0cf00732749ab7f424d0893e6
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-24 07:45:24 +0300

    feat:add solid file #7 task #10

--- HomeWork/7/unit_07/unit_07.js
+++ HomeWork/7/unit_07/unit_07.js
@@ -107,4 +107,12 @@
 //  }
 
 
+// Task 10
+// Создайте функцию t10, которая принимает 2 числа и возвращает большее из них.В случае равенства - любое из чисел.
 
+//  function t10(a, b) {
+//  	return a > b ? a : b;
+//  }
+//  document.querySelector('.b-10').onclick = function () {
+//  	document.querySelector('.out-10').textContent = t10(9, 10);
+//  }
