62d095755526b9ea068f98855a16b7f019ea35f8
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-24 07:13:28 +0300

    feat:add solid file #7 task #5

--- HomeWork/7/unit_07/unit_07.js
+++ HomeWork/7/unit_07/unit_07.js
@@ -50,13 +50,12 @@
 // Напишите функцию t5, которая принимает ваше имя в качестве параметра и возвращает строку Hello name, где name - принятое в качестве параметра имя.
 
 
-function t5(yourName) {
-
-}
-
-document.querySelector('.b-5').onclick = function () {
-	document.querySelector('.out-5').textContent = t5('Alex');
-}
+//  function t5(yourName) {
+//  	return `Hello ${yourName}`;
+//  }
+//  document.querySelector('.b-5').onclick = function () {
+//  	document.querySelector('.out-5').textContent = t5('Alex');
+//  }
 
 
 
