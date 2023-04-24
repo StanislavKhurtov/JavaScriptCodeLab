a7a6456b1675cc4e8d2370f41bbe6a81f9c48b63
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-24 07:22:52 +0300

    feat:add solid file #7 task #6

--- HomeWork/7/unit_07/unit_07.js
+++ HomeWork/7/unit_07/unit_07.js
@@ -62,13 +62,12 @@
 // Task 6
 // Напишите функцию t6, которая принимает 2 числа и возвращает случайное целое число от первого до второго принятого параметра.
 
-function t6(a, b) {
-
-}
-
-document.querySelector('.b-6').onclick = function () {
-	document.querySelector('.out-6').textContent = t6(1, 7);
-}
+//  function t6(a, b) {
+//  	return a > b ? Math.floor(Math.random() * (a - b) + b) : Math.floor(Math.random() * (b - a) + a);
+//  }
+//  document.querySelector('.b-6').onclick = function () {
+//  	document.querySelector('.out-6').textContent = t6(1, 7);
+//  }
 
 
 // Task 7
