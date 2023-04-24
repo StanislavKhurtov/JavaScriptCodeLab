3d56ef4c28aec7d4bde07d849b37d6ff07199e4b
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-24 07:31:09 +0300

    feat:add solid file #7 task #7

--- HomeWork/7/unit_07/unit_07.js
+++ HomeWork/7/unit_07/unit_07.js
@@ -87,13 +87,12 @@
 // Task 8
 // Напишите функцию t8, которая принимает строку в качестве параметра и возвращает результат с очищенными пробелами в начале и вконце строки.Т.е.принимает _hello_(где знак _ символизирует пробел), а возвращает hello.Для удаления пробелов - используйте trim.
 
-function t8(str) {
-
-}
-
-document.querySelector('.b-8').onclick = function () {
-	document.querySelector('.out-8').textContent = t8('  Hello  ');
-}
+// function t8(str) {
+// 	return str.trim();
+// }
+// document.querySelector('.b-8').onclick = function () {
+// 	document.querySelector('.out-8').textContent = t8('  Hello  ');
+// }
 
 
 // Task 9
