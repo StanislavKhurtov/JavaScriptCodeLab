e16673180ef72d4a4a54509633e743518728ae3b
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-27 12:06:13 +0300

    feat: add solid file#8 task #15

--- HomeWork/8/unit_08.js
+++ HomeWork/8/unit_08.js
@@ -314,9 +314,13 @@
 // Кнопка b-15 запускает функцию t15  Функция должна выводить следующую последовательность в out-15:
 // 10 0 9 1 8 2 7 3 6 4 5 5 4 6 3 7 2 8 1 9 0 10
 // Для вывода использовать цикл  while. Разделитель пробел.
-
-function t15() {
-
-}
-
-document.querySelector('.b-15').onclick = t15;
+// let out = "";
+// function t15() {
+// 	let i = 0;
+// 	while (i <= 10) {
+// 		out += (10 - i) + " " + i + " ";
+// 		i++;
+// 	}
+// 	document.querySelector('.out-15').innerHTML = out;
+// }
+// document.querySelector('.b-15').onclick = t15;
