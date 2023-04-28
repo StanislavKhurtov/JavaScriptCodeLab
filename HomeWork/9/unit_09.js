7a0ba0cb59151f3a03249bb8d1025d201d63350b
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-28 14:42:47 +0300

    feat:add solid file #9 task #4

--- HomeWork/9/unit_09.js
+++ HomeWork/9/unit_09.js
@@ -29,7 +29,10 @@
 // По нажатию кнопки b-4 запускайте функцию f4, которая делает toggle класса bg-4 для блока out-4.
 
 
-
+//function f4() {
+//	document.querySelector('.out-4').classList.toggle('bg-4');
+//}
+//document.querySelector('.b-4').onclick = f4;
 
 
 //  Task 5
