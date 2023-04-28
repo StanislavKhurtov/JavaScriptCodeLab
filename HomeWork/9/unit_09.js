2d4bdf363a6e2d897ad57da21e9b04629d4775c5
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-28 15:21:21 +0300

    feat:add solid file #9 task #8

--- HomeWork/9/unit_09.js
+++ HomeWork/9/unit_09.js
@@ -77,13 +77,13 @@
 //  Task 8
 // По нажатию кнопки b-8 запускайте функцию f8, которая делает toggle блокам out-8 класс .bg-8. Обратите внимание, что данных блоков больше одного, следовательно нужен цикл.
 
-let blocks8 = document.querySelectorAll('.out-8');
-
-function f8() {
-	//внутри цикла blocks8[i].classList....
-}
-
-document.querySelector('.b-8').onclick = f8;
+//  let blocks8 = document.querySelectorAll('.out-8');
+//  function f8() {
+//  	for (let i = 0; i < blocks8.length; i++) {
+//  		blocks8[i].classList.toggle('bg-7');
+//  	}
+//  }
+//  document.querySelector('.b-8').onclick = f8;
 
 
 //  Task 9
