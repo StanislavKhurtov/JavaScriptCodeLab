4a99882e359a4ab7985a4951328195190083081f
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-28 15:30:20 +0300

    feat:add solid file #9 task #10

--- HomeWork/9/unit_09.js
+++ HomeWork/9/unit_09.js
@@ -101,14 +101,13 @@
 //  Task 10
 // Усложним предыдущие задачи. С помощью цикла повесьте на блоки out-10 событие клик. По клику должна выполняться функция f10. Функция, должна делать toggle класса bg-10 тому out-10 на котором кликнули.
 
-//let div10 = тут получите все out-10
-
-function f10() {
-
-}
-
-// а тут цикл, похожий на предыдущее задание
-
+//  let div10 = document.querySelectorAll('.out-10');
+//  for (let i = 0; i < div10.length; i++) {
+//  	div10[i].addEventListener('click', f10)
+//  }
+//  function f10() {
+//  	this.classList.toggle('bg-10');
+//  }
 
 
 //  Task 11
