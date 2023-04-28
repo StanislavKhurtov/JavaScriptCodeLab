c507f20e9cba7168b3c51b7b897c224057983008
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-28 16:13:57 +0300

    feat:add solid file #9 task #20

--- HomeWork/9/unit_09.js
+++ HomeWork/9/unit_09.js
@@ -219,8 +219,7 @@
 //  Task 20
 // Добавьте кнопку .b-20, которая запускает функцию f20. Функция с помощью setAttribute присваивает атрибут title="go" в div.out-20.
 
-function f20() {
-
-}
-
-document.querySelector('.b-20').onclick = f20;
+//  function f20() {
+//  	document.querySelector('.out-20').setAttribute('title', "go");
+//  }
+//  document.querySelector('.b-20').onclick = f20;
