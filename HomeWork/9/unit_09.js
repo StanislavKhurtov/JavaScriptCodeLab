a2220d9b7e37d2dcf8681b91a607e4640d0f7180
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-04-28 14:42:27 +0300

    feat:add solid file #9 task #3

--- HomeWork/9/unit_09.js
+++ HomeWork/9/unit_09.js
@@ -20,7 +20,10 @@
 //  Task 3
 // По нажатию кнопки b-3 запускайте функцию f3, которая удаляет у блока out-3 класс .bg-3.
 
-
+//  function f3() {
+//  	document.querySelector('.out-3').classList.remove('bg-3');
+//  }
+//  document.querySelector('.b-3').onclick = f3;
 
 //  Task 4
 // По нажатию кнопки b-4 запускайте функцию f4, которая делает toggle класса bg-4 для блока out-4.
