f64609ed11d441e01605449f9830e77ee7cc5353
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-04 22:03:53 +0300

    feat:add solid file #13 task #16

--- HomeWork/13/unit_13.js
+++ HomeWork/13/unit_13.js
@@ -338,26 +338,28 @@ function f5(arr, block) {
 // Task 16
 // При нажатии b-16 выполняете функцию f16. Функция должна в out-16 выводить элементы name вложенных массивов в a16. Вывод через пробел.
 
-let a16 = {
-	"iis8sj": {
-		"name": "Ivan",
-		"age": 27,
-	},
-	"iiss7j": {
-		"name": "Petr",
-		"age": 26,
-	},
-	"s3s8sj": {
-		"name": "Serg",
-		"age": 47,
-	},
-}
-
-function f16() {
-
-}
-
-document.querySelector('.b-16').onclick = f16;
+//  let a16 = {
+//  	"iis8sj": {
+//  		"name": "Ivan",
+//  		"age": 27,
+//  	},
+//  	"iiss7j": {
+//  		"name": "Petr",
+//  		"age": 26,
+//  	},
+//  	"s3s8sj": {
+//  		"name": "Serg",
+//  		"age": 47,
+//  	},
+//  }
+//  function f16() {
+//  	let out = "";
+//  	for (let key in a16) {
+//  		out += a16[key]["name"] + " ";
+//  	}
+//  	document.querySelector('.out-16').innerHTML = out;
+//  }
+//  document.querySelector('.b-16').onclick = f16;
 
 
 // Task 17
