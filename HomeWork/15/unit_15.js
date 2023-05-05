e4160939b997578124f5039890e8676b34208f16
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-05 21:20:27 +0300

    feat:add solid file #15  task #2

--- HomeWork/15/unit_15.js
+++ HomeWork/15/unit_15.js
@@ -14,6 +14,13 @@ console.log(s1);
 // При нажатии b-2 выполняете функцию f2. Функция должна добавить в набор s2 элементы, которые пользователь вводит в i-2. Функция должна выводить в консоль s2 после каждого добавления элемента.
 
 
+let s2 = new Set();
+const f2 = () => {
+	let inp = document.querySelector('.i-2').value;
+	s2.add(inp);
+	console.log(s2);
+}
+document.querySelector('.b-2').onclick = f2;
 
 
 // Task 3
