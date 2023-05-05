b0d5812ec181aaf0f283545582a2bd4f18aa2576
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-05 10:08:25 +0300

    feat:add solid file #13 task #20

--- HomeWork/13/unit_13.js
+++ HomeWork/13/unit_13.js
@@ -450,4 +450,15 @@ let a20 = {
 	"blue": [['Minska', 1], ['Obolon', 0], ['Pochaina', 2], ['Holosiivska', 0]],
 	"green": [['Syrets', 1], ['Zoloti Vorota', 2], ['Klovska', 0], ['Vidubichi', 1]],
 }
-
+function f20() {
+	let out = "";
+	for (let key in a20) {
+		for (let i = 0; i < a20[key].length; i++) {
+			if (a20[key][i][1] == 2) {
+				out +=a20[key][i][0] + " ";
+			}
+		}
+	}
+	document.querySelector('.out-20').innerHTML = out;
+}
+document.querySelector('.b-20').onclick = f20
