3a78f4e199e762ef326e17b113da5cca51cfea99
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-07 08:47:31 +0300

    feat:add solid file #15  task #9

--- HomeWork/15/unit_15.js
+++ HomeWork/15/unit_15.js
@@ -105,8 +105,13 @@ document.querySelector('.b-8').onclick = f8;
 //  При нажатии b-9 выполняете функцию f9. Функция должна принимать набор our_set в качестве параметра, преобразовывать его в строку, причем после каждого символа строки должен быть пробел. Функция должна возвращать результирующую строку.
 // В нашем примере результат должен быть 9 8 7 6 5
 
-const f9 = our_set => { }
-
+const f9 = our_set => {
+	let out = "";
+	for (let item of our_set) {
+		out += item + " ";
+	}
+	return out;
+}
 document.querySelector('.b-9').onclick = () => {
 	let s9 = new Set([9, 8, 7, 6, 5]);
 	document.querySelector('.out-9').innerHTML = f9(s9);
