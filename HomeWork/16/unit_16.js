79abb46679ead043a997010e0c58b5ccda5ab4ca
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-07 15:59:19 +0300

    feat:add solid file #16 task #12

--- HomeWork/16/unit_16.js
+++ HomeWork/16/unit_16.js
@@ -188,7 +188,11 @@ document.querySelector('.b-11').addEventListener('click', f11);
 let a12 = [4, 5, 6, 7];
 
 function f12() {
-
+	let out = "";
+	for (let el of a12) {
+		out += el + " ";
+	}
+	document.querySelector('.out-12').innerHTML = out;
 }
 
 document.querySelector('.b-12').addEventListener('click', f12);
