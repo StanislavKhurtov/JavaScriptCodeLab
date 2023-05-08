e7975ceadc9eb83cbc7657dda221d6a88314bf12
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-08 14:25:41 +0300

    feat:add solid file #17 task #6

--- HomeWork/17/unit_17.js
+++ HomeWork/17/unit_17.js
@@ -83,7 +83,6 @@ document.querySelector('.b-4').onclick = () => {
 /*  Дан массив b1 = [3, 14, 15, 92]. C помощью filter переберите массив и создайте b1_res, который содержит только четные числа из b1. Возвратите b1_res. Действия должны запускаться при вызове функции t5. */
 
 let b1 = [3, 14, 15, 92];
-
 function t5() {
 	b1_res = b1.filter((item) => {
 		if (item % 2 == 0) {
@@ -92,8 +91,6 @@ function t5() {
 	})
 	return b1_res;
 }
-
-
 document.querySelector('.b-5').onclick = () => {
 	console.log(t5());
 }
@@ -106,7 +103,8 @@ document.querySelector('.b-5').onclick = () => {
 let b6 = [3, 14, 15, 92, "6", "5", "hello", 32];
 
 function t6() {
-
+	b6_res = b6.filter(item => item % 2 == 0 && typeof item === "number");
+	return b6_res;
 }
 
 document.querySelector('.b-6').onclick = () => {
