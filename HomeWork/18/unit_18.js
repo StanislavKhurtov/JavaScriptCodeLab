faa798b1402ce055a2de7c24dd525f85e8de659f
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-11 19:49:07 +0300

    feat: add solid file #18 task #7

--- HomeWork/18/unit_18.js
+++ HomeWork/18/unit_18.js
@@ -84,7 +84,9 @@ document.querySelector('.b-6').onclick = t6;
 /*  Дана строка str7='hello world hi mazai' - преобразуйте ее в массив и разбейте по словам. Причем слова не должны содержать пробелов и присвойте a6_res. выведите на страницу.  Запускаться решение должно при вызове функции t7. */
 
 function t7() {
-
+	let str7='hello world hi mazai';
+	a7_res = str7.split(" ");
+	document.querySelector(".out-7").innerHTML = a7_res;
 }
 
 document.querySelector('.b-7').onclick = t7;
