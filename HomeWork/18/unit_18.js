35a279f874feea9d965b6c2af8db0c63a2c3eca1
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-11 19:33:35 +0300

    feat: add solid file #18 task #5

--- HomeWork/18/unit_18.js
+++ HomeWork/18/unit_18.js
@@ -59,7 +59,8 @@ document.querySelector('.b-4').onclick = t4;
 /*  На странице созданы 3 p.task-5 c атрибутом data. С помощью forEach переберите их и добавьте событие клик. Напишите функцию t5, которая будет запускаться при клике и добавлять атрибут data элемента, по которому кликнули в массив a5_res.*/
 
 function t5() {
-
+	a5_res.push(this.getAttribute('data'));
+	document.querySelector(".out-5").innerHTML = a5_res;
 }
 
 document.querySelectorAll('.task-5').forEach(elem => {
