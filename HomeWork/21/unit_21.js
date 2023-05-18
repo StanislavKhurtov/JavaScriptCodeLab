9e396a27b80fd7e2d14879ae0cd7aab8118e8908
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-05-18 22:35:05 +0300

    feat: add file #21 task #6

--- HomeWork/21/unit_21.js
+++ HomeWork/21/unit_21.js
@@ -60,10 +60,10 @@ document.querySelector('.b-5').addEventListener('touchstart', t5)
 /*  Добавьте событие ontouchend на div-4. При его срабатывании выведите в out-6 слово touchend. */
 
 function t6() {
-
+	document.querySelector('.out-6').innerHTML = 'touchend';
 }
 
-// ваше событие здесь!!!
+document.querySelector('.div-4').ontouchend = t6;
 
 
 // Task 7 ============================================
